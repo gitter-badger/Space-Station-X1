b08adf7bbe14de0165368d1040d710971d230c40
AlmostPete <almostpete@gmail.com>
2015-07-05 19:56:33 -0600

    Started the server, got the timing and main loop in

---
+++ SSServer/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using LoopTimer = SSCyg.Server.Utility._ServerLoopTimer;
+
+namespace SSCyg.Server
+{
+	public class Server
+	{
+		#region Members
+		// If the server is active
+		public bool IsActive { get; private set; }
+
+		// The timer that fires the main loop
+		private LoopTimer _loopTimer;
+		#endregion
+
+		// Initializes all of the server services
+		public Server()
+		{
+			IsActive = false;
+		}
+
+		// Loads in the server settings and opens the server for connections
+		public void Start()
+		{
+			IsActive = true;
+		}
+
+		#region Server Loop
+		// Launches the server into its main loop, but is not the main loop itself
+		public void MainLoop()
+		{
+			// Try to run the loop every 1 msec
+			_loopTimer = new LoopTimer(() => { RunLoop(); }, 1);
+
+			// Wait for the event to be signaled and try to run the loop
+			while (IsActive)
+			{
+				_loopTimer.WaitOne();
+
+				mainLoopStuff();
+			}
+		}
+
+		// Sets the event to cause the loop to run
+		public void RunLoop()
+		{
+			_loopTimer.Set();
+		}
+		
+		// The actual main loop
+		private void mainLoopStuff()
+		{
+
+		}
+		#endregion
+	}
+}
---
+++ SSServer/Utility/_ServerLoopTimer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Timer = System.Timers.Timer;
+using ARE = System.Threading.AutoResetEvent;
+
+namespace SSCyg.Server.Utility
+{
+	// Maintains the timer and event handling for smooth updating
+	internal class _ServerLoopTimer
+	{
+		private delegate void MainServerLoop();
+
+		private Timer _timer;
+		private ARE _are;
+
+		public _ServerLoopTimer(MainServerLoop loop, uint period)
+		{
+			_timer = new Timer(period);
+			_timer.Elapsed += (sender, e) => { loop(); };
+			_are = new ARE(true);
+			_timer.Enabled = true;
+		}
+
+		// Sets the AutoResetEvent
+		public void Set()
+		{
+			_are.Set();
+		}
+
+		// Waits the AutoResetEvent
+		public void WaitOne()
+		{
+			_are.WaitOne(-1);
+		}
+	}
+}
